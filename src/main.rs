f6640170fd0df1af12f68fd76fbf03ce1f369e47
Johannes <freelon@gmx.net>
2022-12-01 11:00:15 +0100

    Day 01 part 2.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs::read_to_string;
 
 fn main() {
     let input = read_to_string("input/day01.txt").unwrap();
-    let m: Option<i32> = input.split("\n\n").map(|elve| elve.lines().map(|line| line.parse::<i32>().unwrap()).sum()).max();
-    println!("{:?}", m);
+    let mut snacks: Vec<i32> = input.split("\n\n").map(|elve| elve.lines().map(|line| line.parse::<i32>().unwrap()).sum()).collect();
+    snacks.sort();
+    let x: i32 = snacks.iter().rev().take(3).sum();
+    println!("{}", x);
 }
