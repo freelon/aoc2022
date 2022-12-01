7de68b73d83e2c97f68380f7499e01dc9a7b0bc4
Johannes <freelon@gmx.net>
2022-12-01 10:57:16 +0100

    Day 01 part 1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input/day01.txt").unwrap();
+    let m: Option<i32> = input.split("\n\n").map(|elve| elve.lines().map(|line| line.parse::<i32>().unwrap()).sum()).max();
+    println!("{:?}", m);
 }
