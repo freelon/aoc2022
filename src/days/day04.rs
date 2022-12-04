fb57a87370cd9ef3ca747dabb0d0d98e138f3ba6
Johannes <freelon@gmx.net>
2022-12-04 11:55:45 +0100

    Refactored day 04

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -6,46 +6,47 @@ pub fn create(input: String) -> Box<dyn Day> {
     Box::new(Day04 { input })
 }
 
+type R = RangeInclusive<u32>;
+
 struct Day04 {
     input: String,
 }
 
 impl Day for Day04 {
     fn part1(&self) -> String {
-        let fully_contained = self.count_filtered_pairs(|(a, b)| fully_contains(a, b) || fully_contains(b, a));
+        let fully_contained = self.count_filtered_pairs(fully_contains);
         format!("{fully_contained}")
     }
 
     fn part2(&self) -> String {
-        let partial_contained = self.count_filtered_pairs(|(a, b)| partially_contains(a, b) || partially_contains(b, a));
+        let partial_contained = self.count_filtered_pairs(partially_contains);
         format!("{partial_contained}")
     }
 }
 
 impl Day04 {
-    fn count_filtered_pairs(&self, filter: for<'r> fn(&'r (RangeInclusive<u32>, RangeInclusive<u32>)) -> bool) -> usize {
-        self
-            .input
+    fn count_filtered_pairs(&self, filter: impl Fn(&R, &R) -> bool) -> usize {
+        self.input
             .lines()
             .map(|line| {
                 let (left, right) = line.split_once(",").unwrap();
                 (to_range(left), to_range(right))
             })
-            .filter(filter)
+            .filter(|(a, b)| filter(a, b) || filter(b, a))
             .count()
     }
 }
 
-fn to_range(s: &str) -> RangeInclusive<u32> {
+fn to_range(s: &str) -> R {
     let (from, to) = s.split_once("-").unwrap();
     let (from, to): (u32, u32) = (from.parse().unwrap(), to.parse().unwrap());
     from..=to
 }
 
-fn fully_contains(a: &RangeInclusive<u32>, b: &RangeInclusive<u32>) -> bool {
+fn fully_contains(a: &R, b: &R) -> bool {
     a.contains(b.start()) && a.contains(b.end())
 }
 
-fn partially_contains(a: &RangeInclusive<u32>, b: &RangeInclusive<u32>) -> bool {
+fn partially_contains(a: &R, b: &R) -> bool {
     a.contains(b.start()) || a.contains(b.end())
 }
