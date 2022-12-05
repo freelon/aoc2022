16aee6c5b8eb1cf24a325feca01b7f0f37d3598c
Johannes <freelon@gmx.net>
2022-12-05 17:12:21 +0100

    Day 05 part 2 taking multiple values at once

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,3 +1,5 @@
+#![feature(slice_take)]
+
 use crate::days::Day;
 
 pub fn create(input: String) -> Box<dyn Day> {
@@ -15,7 +17,11 @@ impl Day for DayXX {
 
         for command in moves.lines() {
             let parts: Vec<&str> = command.split(" ").collect();
-            let (times, from, to): (usize, usize, usize) = (parts[1].parse().unwrap(), parts[3].parse().unwrap(), parts[5].parse().unwrap());
+            let (times, from, to): (usize, usize, usize) = (
+                parts[1].parse().unwrap(),
+                parts[3].parse().unwrap(),
+                parts[5].parse().unwrap(),
+            );
             for _ in 0..times {
                 let cargo = stacks[from - 1].pop().unwrap();
                 stacks[to - 1].push(cargo);
@@ -32,16 +38,15 @@ impl Day for DayXX {
 
         for command in moves.lines() {
             let parts: Vec<&str> = command.split(" ").collect();
-            let (times, from, to): (usize, usize, usize) = (parts[1].parse().unwrap(), parts[3].parse().unwrap(), parts[5].parse().unwrap());
+            let (times, from, to): (usize, usize, usize) = (
+                parts[1].parse().unwrap(),
+                parts[3].parse().unwrap(),
+                parts[5].parse().unwrap(),
+            );
 
-            let mut temp = Vec::new();
-            for _ in 0..times {
-                let cargo = stacks[from - 1].pop().unwrap();
-                temp.push(cargo);
-            }
-            for cargo in temp.into_iter().rev() {
-                stacks[to - 1].push(cargo);
-            }
+            let from = stacks.get_mut(from - 1).unwrap();
+            let mut temp = from.split_off(from.len() - times);
+            stacks[to - 1].append(&mut temp);
         }
 
         let result: String = stacks.iter().map(|stack| stack.last().unwrap()).collect();
@@ -58,9 +63,7 @@ impl DayXX {
         let highest_stack = stacks.len() - 1; // last line is the stack number
         for y in (0..highest_stack).rev() {
             for stack in 0..stacks.len() {
-                let cargo = lines[y]
-                    .chars()
-                    .nth(stack * 4 + 1).unwrap();
+                let cargo = lines[y].chars().nth(stack * 4 + 1).unwrap();
                 if cargo.is_alphabetic() {
                     stacks[stack].push(cargo);
                 }
