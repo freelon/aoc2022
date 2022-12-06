8c004b52ed375a38f4153ac8224bd47c561adeec
Johannes <freelon@gmx.net>
2022-12-06 06:12:04 +0100

    1

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,3 +1,7 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
 use crate::days::Day;
 
 pub fn create(input: String) -> Box<dyn Day> {
@@ -10,7 +14,15 @@ struct Day06 {
 
 impl Day for Day06 {
     fn part1(&self) -> String {
-        format!("")
+        let foo = self.input.chars().tuple_windows::<(char, char, char, char)>().enumerate().find(|(index, chars)| {
+            let mut set = HashSet::new();
+            set.insert(chars.0);
+            set.insert(chars.1);
+            set.insert(chars.2);
+            set.insert(chars.3);
+            set.len() == 4
+        }).unwrap();
+        format!("{}", foo.0 + 4)
     }
 
     fn part2(&self) -> String {
