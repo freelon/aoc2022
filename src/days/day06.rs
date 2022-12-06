7be15e7871a7273e6a8c330d12f0f58187b78ea5
Johannes <freelon@gmx.net>
2022-12-06 07:15:52 +0100

    Day 06 much faster

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,5 +1,3 @@
-use std::collections::{HashSet, VecDeque};
-
 use itertools::FoldWhile::{Continue, Done};
 use itertools::Itertools;
 
@@ -27,22 +25,25 @@ impl Day for Day06 {
 
 impl Day06 {
     fn consumed_until_different(&self, size: usize) -> usize {
-        let slice: VecDeque<char> = self.input.chars().take(size).collect();
-        let foo = self
-            .input
-            .chars()
-            .skip(size)
-            .fold_while((size, slice), |(consumed, mut slice), next| {
-                let set: HashSet<_> = slice.iter().collect();
-                if set.len() < size {
-                    slice.pop_front();
-                    slice.push_back(next);
-                    Continue((consumed + 1, slice))
-                } else {
-                    Done((consumed, slice))
+        let vec: Vec<_> = self.input.chars().collect();
+        let foo = vec
+            .windows(size)
+            .fold_while(size, |consumed, next| {
+                if unique_chars(next) {
+                    return Done(consumed);
                 }
+                Continue(consumed + 1)
             })
             .into_inner();
-        foo.0
+        foo
     }
+}
+
+fn unique_chars(chars: &[char]) -> bool {
+    for i in 1..chars.len() {
+        if chars[0..i].contains(&chars[i]) {
+            return false;
+        }
+    }
+    true
 }
