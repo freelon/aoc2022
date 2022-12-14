308786f0ac75576624189f0610a76ca251e61d10
Johannes <freelon@gmx.net>
2022-12-14 10:50:11 +0100

    Day 14 part 2

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -26,31 +26,13 @@ impl Day for Day14 {
     fn part1(&self) -> String {
         let mut map = self.load();
         play(&mut map);
-
-        let xmin = map.keys().min_by_key(|it| it.0).unwrap().0;
-        let xmax = map.keys().max_by_key(|it| it.0).unwrap().0;
-        let ymin = map.keys().min_by_key(|it| it.1).unwrap().1;
-        let ymax = map.keys().max_by_key(|it| it.1).unwrap().1;
-
-        for y in ymin..=ymax {
-            for x in xmin..=xmax {
-                let c = match map.get(&(x, y)) {
-                    Some(t) => match t {
-                        Sand => 'o',
-                        Rock => '#',
-                    },
-                    None => '.',
-                };
-                print!("{c}");
-            }
-            println!();
-        }
-
         map.values().filter(|it| **it == Sand).count().to_string()
     }
 
     fn part2(&self) -> String {
-        format!("")
+        let mut map = self.load();
+        play2(&mut map);
+        map.values().filter(|it| **it == Sand).count().to_string()
     }
 }
 
@@ -60,7 +42,7 @@ fn play(map: &mut HashMap<P, Type>) {
         let mut s = (500, 0);
         loop {
             let d = directions(s);
-            let d = d.into_iter().find(|next| map.get(&next).is_none());
+            let d = d.into_iter().find(|next| map.get(next).is_none());
             if let Some(next) = d {
                 s = next;
 
@@ -74,7 +56,31 @@ fn play(map: &mut HashMap<P, Type>) {
             }
         }
     }
-} // 647 too low
+}
+
+fn play2(map: &mut HashMap<P, Type>) {
+    let depth = *map.keys().map(|(_, y)| y).max().unwrap();
+    'outer: loop {
+        let mut s = (500, 0);
+        loop {
+            let d = directions(s);
+            let d = d
+                .into_iter()
+                .find(|next| map.get(next).is_none() && next.1 < depth + 2);
+            if let Some(next) = d {
+                s = next;
+            } else {
+                map.insert(s, Sand);
+
+                if s == (500, 0) {
+                    break 'outer;
+                }
+
+                continue 'outer;
+            }
+        }
+    }
+}
 
 fn directions(p: P) -> Vec<P> {
     vec![(p.0, p.1 + 1), (p.0 - 1, p.1 + 1), (p.0 + 1, p.1 + 1)]
@@ -97,14 +103,36 @@ impl Day14 {
                 let mut xc = xa;
                 let mut yc = ya;
                 while (xc, yc) != (xb, yb) {
-                    map.insert((xc, yc), Type::Rock);
+                    map.insert((xc, yc), Rock);
                     xc += dx;
                     yc += dy;
                 }
-                map.insert((xc, yc), Type::Rock);
+                map.insert((xc, yc), Rock);
             }
         }
         map
+    }
+}
+
+#[allow(dead_code)]
+fn print_map(map: &HashMap<P, Type>) {
+    let x_min = map.keys().min_by_key(|it| it.0).unwrap().0;
+    let x_max = map.keys().max_by_key(|it| it.0).unwrap().0;
+    let y_min = map.keys().min_by_key(|it| it.1).unwrap().1;
+    let y_max = map.keys().max_by_key(|it| it.1).unwrap().1;
+
+    for y in y_min..=y_max {
+        for x in x_min..=x_max {
+            let c = match map.get(&(x, y)) {
+                Some(t) => match t {
+                    Sand => 'o',
+                    Rock => '#',
+                },
+                None => '.',
+            };
+            print!("{c}");
+        }
+        println!();
     }
 }
 
