b34d9563ef353edb66284506b420ba00cace3d7d
Johannes <freelon@gmx.net>
2022-12-19 23:34:10 +0100

    Day 19 part 2

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 use itertools::Itertools;
 use rayon::prelude::*;
 
@@ -13,6 +15,30 @@ struct Day19 {
 
 impl Day for Day19 {
     fn part1(&self) -> String {
+        let factories = self.parse();
+
+        factories
+            .par_iter()
+            .map(|factory| factory.0 as i32 * score(*factory, 24) as i32)
+            .sum::<i32>()
+            .to_string()
+    }
+
+    fn part2(&self) -> String {
+        // 218 too low, 176088 too high
+        let factories = self.parse();
+
+        factories
+            .par_iter()
+            .take(3)
+            .map(|factory| score(*factory, 32) as i32)
+            .product::<i32>()
+            .to_string()
+    }
+}
+
+impl Day19 {
+    fn parse(&self) -> Vec<(i32, i32, i32, i32, i32, i32, i32)> {
         let factories =
             self.input
                 .lines()
@@ -41,17 +67,7 @@ impl Day for Day19 {
                     )
                 })
                 .collect_vec();
-
         factories
-            .par_iter()
-            .map(|factory| factory.0 as i32 * score(*factory) as i32)
-            .inspect(|it| println!("{it}"))
-            .sum::<i32>()
-            .to_string()
-    }
-
-    fn part2(&self) -> String {
-        format!("")
     }
 }
 
@@ -65,6 +81,7 @@ fn score(
         i32,
         i32,
     ),
+    minutes: i32,
 ) -> i32 {
     let robots = [1, 0, 0, 0];
     let resources = [0, 0, 0, 0];
@@ -74,18 +91,24 @@ fn score(
     let cost_obsidian_robot = [-obsidian_ore, -obsidian_clay, 0, 0];
     let cost_geode_robot = [-geode_ore, 0, -geode_obsidian, 0];
 
-    let foo = [cost_ore_robot, cost_clay_robot, cost_obsidian_robot, cost_geode_robot];
+    let costs = [
+        cost_ore_robot,
+        cost_clay_robot,
+        cost_obsidian_robot,
+        cost_geode_robot,
+    ];
     let max_costs = [
-        max_cost_of_resource(&foo, 0),
-        max_cost_of_resource(&foo, 1),
-        max_cost_of_resource(&foo, 2),
-        0
+        max_cost_of_resource(&costs, 0),
+        max_cost_of_resource(&costs, 1),
+        max_cost_of_resource(&costs, 2),
+        0,
     ];
 
+    let mut max_so_far = 0;
     solve(
         robots,
         resources,
-        24,
+        minutes,
         &[
             ([0, 0, 0, 0], noop),
             ([1, 0, 0, 0], cost_ore_robot),
@@ -94,15 +117,31 @@ fn score(
             ([0, 0, 0, 1], cost_geode_robot),
         ],
         &max_costs,
+        &mut max_so_far,
     )
 }
 
-fn solve(robots: Stuff, resources: Stuff, minutes: i32, costs: &[(Stuff, Stuff)], max_cost: &Stuff) -> i32 {
-    if minutes == 1 {
-        return add(&resources, &robots)[3];
+fn solve(
+    robots: Stuff,
+    resources: Stuff,
+    minutes: i32,
+    costs: &[(Stuff, Stuff)],
+    max_cost: &Stuff,
+    max_so_far: &mut i32,
+) -> i32 {
+    let max_possible_geodes_upper: i32 =
+        resources[3] + (robots[3]..(robots[3] + minutes)).sum::<i32>();
+    if max_possible_geodes_upper <= *max_so_far {
+        return 0;
     }
 
-    costs
+    if minutes == 1 {
+        let v = add(&resources, &robots)[3];
+        *max_so_far = max(*max_so_far, v);
+        return v;
+    }
+
+    let v = costs
         .iter()
         .enumerate()
         .filter_map(|(index, (robot_to_add, cost))| {
@@ -112,7 +151,6 @@ fn solve(robots: Stuff, resources: Stuff, minutes: i32, costs: &[(Stuff, Stuff)]
             }
 
             if index > 0 && index < 3 && max_cost[index - 1] <= robots[index - 1] {
-                //println!("at {minutes}: {:?} - {index} {:?} ", robots, costs);
                 return None;
             }
 
@@ -122,10 +160,20 @@ fn solve(robots: Stuff, resources: Stuff, minutes: i32, costs: &[(Stuff, Stuff)]
             }
             let new_robots = add(&robots, robot_to_add);
 
-            Some(solve(new_robots, res_after_harvest, minutes - 1, costs, max_cost))
+            Some(solve(
+                new_robots,
+                res_after_harvest,
+                minutes - 1,
+                costs,
+                max_cost,
+                max_so_far,
+            ))
         })
         .max()
-        .unwrap()
+        .unwrap();
+
+    *max_so_far = max(*max_so_far, v);
+    v
 }
 
 type Stuff = [i32; 4];
@@ -153,18 +201,45 @@ mod test {
     use crate::days::day19::Day19;
 
     #[test]
-    fn score() {
-        assert_eq!(super::score((1, 4, 2, 3, 14, 2, 7)), 9);
+    fn score_a1() {
+        assert_eq!(super::score((1, 4, 2, 3, 14, 2, 7), 24), 9);
     }
 
     #[test]
-    fn score_b() {
-        assert_eq!(super::score((2, 2, 3, 3, 8, 3, 12)), 12);
+    fn score_a2() {
+        assert_eq!(super::score((1, 4, 2, 3, 14, 2, 7), 32), 56);
+    }
+
+    #[test]
+    fn score_b1() {
+        assert_eq!(super::score((2, 2, 3, 3, 8, 3, 12), 24), 12);
+    }
+
+    #[test]
+    fn score_b2() {
+        assert_eq!(super::score((2, 2, 3, 3, 8, 3, 12), 32), 62);
     }
 
     #[test]
     fn part1() {
-        assert_eq!(Day19 { input: INPUT.to_string() }.part1(), "33");
+        assert_eq!(
+            Day19 {
+                input: INPUT.to_string()
+            }
+                .part1(),
+            "33"
+        );
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(
+            Day19 {
+                input: INPUT.to_string()
+            }
+                .part2(),
+            (56 * 62).to_string()
+        );
     }
 
     const INPUT: &str = "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
