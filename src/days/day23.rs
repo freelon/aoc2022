5501d82716ea21771372e66dc65bc11893c8e2bb
Johannes <freelon@gmx.net>
2022-12-23 21:58:04 +0100

    Day 23 part 2

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -74,13 +74,72 @@ impl Day for Day23 {
 
         let number_of_points = (x_max - x_min + 1) * (y_max - y_min + 1);
         (number_of_points - elves.len() as i32).to_string()
-    } // 4173 too high
+    }
 
     fn part2(&self) -> String {
-        format!("")
+        let mut elves: HashSet<P> = self
+            .input
+            .lines()
+            .enumerate()
+            .flat_map(|(y, line)| {
+                line.chars()
+                    .enumerate()
+                    .filter(|(_, c)| *c == '#')
+                    .map(move |(x, _)| (x as i32, y as i32))
+            })
+            .collect();
+
+        let mut proposed_moves: FxHashMap<P, usize> = FxHashMap::default();
+        let mut target_directions: Vec<(Box<fn(&HashSet<P>, &P) -> bool>, Box<fn(&P) -> P>)> = vec![
+            (Box::new(north_empty), Box::new(north)),
+            (Box::new(south_empty), Box::new(south)),
+            (Box::new(west_empty), Box::new(west)),
+            (Box::new(east_empty), Box::new(east)),
+        ];
+        for round in 1..(i32::MAX) {
+            proposed_moves.clear();
+            elves.iter().for_each(|elf| {
+                let target = Self::target_of(&elves, &target_directions, elf);
+
+                if let Some(p) = target {
+                    *proposed_moves.entry(p).or_insert(0) += 1;
+                }
+            });
+
+            elves = elves
+                .iter()
+                .map(|elf| {
+                    let target = Self::target_of(&elves, &target_directions, &elf);
+
+                    if let Some(target) = target {
+                        if proposed_moves.get(&target).unwrap() == &1 {
+                            return target;
+                        }
+                    }
+
+                    *elf
+                })
+                .collect();
+
+            if proposed_moves.is_empty() {
+                return round.to_string();
+            }
+
+            let f1 = target_directions.remove(0);
+            target_directions.push(f1);
+        }
+
+        let x_min = elves.iter().min_by_key(|elf| elf.0).unwrap().0;
+        let x_max = elves.iter().max_by_key(|elf| elf.0).unwrap().0;
+        let y_min = elves.iter().min_by_key(|elf| elf.1).unwrap().1;
+        let y_max = elves.iter().max_by_key(|elf| elf.1).unwrap().1;
+
+        let number_of_points = (x_max - x_min + 1) * (y_max - y_min + 1);
+        (number_of_points - elves.len() as i32).to_string()
     }
 }
 
+#[allow(dead_code)]
 fn print_map(elves: &HashSet<P>) {
     let x_min = elves.iter().min_by_key(|elf| elf.0).unwrap().0;
     let x_max = elves.iter().max_by_key(|elf| elf.0).unwrap().0;
@@ -168,6 +227,17 @@ mod test {
             }
                 .part1(),
             "110"
+        );
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(
+            Day23 {
+                input: INPUT.to_string()
+            }
+                .part2(),
+            "20"
         );
     }
 
