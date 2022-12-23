4c03aab5c891ac6c9857563dbf11daa2f88f4814
Johannes <freelon@gmx.net>
2022-12-24 00:45:27 +0100

    Day 21 part 2

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -24,8 +24,95 @@ impl Day for Day21 {
     }
 
     fn part2(&self) -> String {
-        format!("")
+        let monkeys: HashMap<&str, &str> = self
+            .input
+            .lines()
+            .map(|line| line.split_once(": ").unwrap())
+            .collect();
+
+        let (left, _, right) = monkeys["root"].split(' ').collect_tuple().unwrap();
+        let mut results: HashMap<&str, i64> = HashMap::new();
+        let left_result = pre_solve2(&monkeys, &mut results, left);
+        let right_result = pre_solve2(&monkeys, &mut results, right);
+
+        if left_result.is_none() && right_result.is_none() {
+            panic!("fuck^^")
+        }
+
+        if left_result.is_none() {
+            solve2(&monkeys, &results, left, right_result.unwrap())
+        } else {
+            solve2(&monkeys, &results, right, left_result.unwrap())
+        }
+            .to_string()
     }
+}
+
+fn solve2(
+    monkeys: &HashMap<&str, &str>,
+    results: &HashMap<&str, i64>,
+    monkey: &str,
+    needed: i64,
+) -> i64 {
+    if monkey == "humn" {
+        return needed;
+    }
+    let stuff = monkeys[monkey];
+    let (lhs, op, rhs) = stuff.split(' ').collect_tuple().unwrap();
+    if let Some(value) = results.get(lhs) {
+        let needed_from_right = match op {
+            "+" => needed - *value,
+            "-" => -(needed - *value),
+            "*" => needed / *value,
+            "/" => *value / needed,
+            _ => panic!("unknown character"),
+        };
+        return solve2(monkeys, results, rhs, needed_from_right);
+    }
+
+    if let Some(value) = results.get(rhs) {
+        let needed_from_left = match op {
+            "+" => needed - *value,
+            "-" => needed + *value,
+            "*" => needed / *value,
+            "/" => needed * *value,
+            _ => panic!("unknown character"),
+        };
+        return solve2(monkeys, results, lhs, needed_from_left);
+    }
+
+    unreachable!()
+}
+
+fn pre_solve2<'a>(
+    monkeys: &HashMap<&'a str, &'a str>,
+    results: &mut HashMap<&'a str, i64>,
+    monkey: &'a str,
+) -> Option<i64> {
+    if monkey == "humn" {
+        return None;
+    }
+    let stuff = monkeys[monkey];
+    let result = if let Some((lhs, op, rhs)) = stuff.split(' ').collect_tuple() {
+        let lhs = pre_solve2(monkeys, results, lhs);
+        let rhs = pre_solve2(monkeys, results, rhs);
+        if lhs.is_none() || rhs.is_none() {
+            return None;
+        }
+
+        match op {
+            "+" => lhs.unwrap() + rhs.unwrap(),
+            "-" => lhs.unwrap() - rhs.unwrap(),
+            "*" => lhs.unwrap() * rhs.unwrap(),
+            "/" => lhs.unwrap() / rhs.unwrap(),
+            _ => panic!("unknown character"),
+        }
+    } else {
+        stuff.parse().unwrap()
+    };
+
+    results.insert(monkey, result);
+    Some(result)
 }
 
 fn solve(monkeys: &HashMap<&str, &str>, monkey: &str) -> i64 {
@@ -38,7 +125,7 @@ fn solve(monkeys: &HashMap<&str, &str>, monkey: &str) -> i64 {
             "-" => lhs - rhs,
             "*" => lhs * rhs,
             "/" => lhs / rhs,
-            _ => panic!("unknown character")
+            _ => panic!("unknown character"),
         }
     } else {
         stuff.parse().unwrap()
