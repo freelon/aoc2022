f3a316a0683a1bd068b077b2b45c7a7a1c213b59
Johannes <freelon@gmx.net>
2023-05-20 21:39:55 +0200

    resolved compiler warnings

--- src/days/day25.rs
+++ src/days/day25.rs
@@ -8,16 +8,6 @@ struct Day25 {
     input: String,
 }
 
-fn convert_from_snafu(input: &str) -> i64 {
-    input
-        .chars()
-        .map(|c| digit(c))
-        .rev()
-        .enumerate()
-        .map(|(i, d)| 5i64.pow(i as u32) * d)
-        .sum()
-}
-
 fn digit(c: char) -> i64 {
     match c {
         '2' => 2,
@@ -94,43 +84,9 @@ fn add_snafu(a: &str, b: &str) -> String {
 
     println!("result {result}");
     //  if result.chars().next().unwrap() == '0' {
-//        result.remove(0);
+    //        result.remove(0);
     //}
     result
-}
-
-fn to_snafu(n: i64) -> String {
-    println!("\nn: {n}");
-    let mut remainder = n;
-    let mut i = 0;
-
-    let mut digits = String::new();
-
-    let mut increment = false;
-    while remainder > 0 {
-        let v = 5i64.pow(i + 1);
-        println!("remainder: {remainder}, v: {v}, i: {i}, increment: {increment}");
-        let mut x = remainder % v;
-        if increment {
-            x += 1;
-            increment = false;
-        }
-        println!("x: {x}");
-
-        if x > 2 {
-            x -= 5;
-            increment = true;
-        }
-        println!("x': {x}");
-
-        remainder -= 5i64.pow(i) * x;
-
-        let s = snafu(x);
-        digits.push(s);
-        i += 1;
-    }
-
-    digits.chars().rev().collect()
 }
 
 fn snafu(x: i64) -> char {
@@ -149,44 +105,8 @@ fn snafu(x: i64) -> char {
 
 #[cfg(test)]
 mod test {
+    use crate::days::day25::{add_snafu, Day25};
     use crate::days::Day;
-    use crate::days::day25::{add_snafu, convert_from_snafu, Day25, to_snafu};
-
-    #[test]
-    fn snafu_to_10() {
-        assert_eq!(convert_from_snafu("1=-0-2"), 1747);
-        assert_eq!(convert_from_snafu("12111"), 906);
-        assert_eq!(convert_from_snafu("2=0="), 198);
-        assert_eq!(convert_from_snafu("21"), 11);
-        assert_eq!(convert_from_snafu("2=01"), 201);
-        assert_eq!(convert_from_snafu("111"), 31);
-        assert_eq!(convert_from_snafu("20012"), 1257);
-        assert_eq!(convert_from_snafu("112"), 32);
-        assert_eq!(convert_from_snafu("1=-1="), 353);
-        assert_eq!(convert_from_snafu("1-12"), 107);
-        assert_eq!(convert_from_snafu("12"), 7);
-        assert_eq!(convert_from_snafu("1="), 3);
-        assert_eq!(convert_from_snafu("122"), 37);
-    }
-
-    #[test]
-    fn decimal_to_snafu() {
-        assert_eq!(to_snafu(1), "1");
-        assert_eq!(to_snafu(2), "2");
-        assert_eq!(to_snafu(3), "1=");
-        assert_eq!(to_snafu(4), "1-");
-        assert_eq!(to_snafu(5), "10");
-        assert_eq!(to_snafu(6), "11");
-        assert_eq!(to_snafu(7), "12");
-        assert_eq!(to_snafu(8), "2=");
-        assert_eq!(to_snafu(9), "2-");
-        assert_eq!(to_snafu(10), "20");
-        assert_eq!(to_snafu(15), "1=0");
-        assert_eq!(to_snafu(20), "1-0");
-        assert_eq!(to_snafu(2022), "1=11-2");
-        assert_eq!(to_snafu(12345), "1-0---0");
-        assert_eq!(to_snafu(314159265), "1121-1110-1=0");
-    }
 
     #[test]
     fn add() {
