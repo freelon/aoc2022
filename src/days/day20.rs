f9f296dcd4a74f8ad1ecac009f02b79f62b278c3
Johannes <freelon@gmx.net>
2023-05-20 16:37:32 +0200

    Day 20 part 2

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -46,7 +46,37 @@ impl Day for Day20 {
     }
 
     fn part2(&self) -> String {
-        format!("")
+        let mut v: VecDeque<(usize, i64)> = self
+            .input
+            .lines()
+            .enumerate()
+            .map(|(i, s)| (i, 811589153 * s.parse::<i64>().unwrap()))
+            .collect();
+
+        for order in (0..v.len()).cycle().take(10 * v.len()) {
+            while v.front().unwrap().0 != order {
+                v.rotate_left(1);
+            }
+            // println!("pulled {order} to front: {v:?}");
+            let (index, value) = v.pop_front().unwrap();
+            if value >= 0 {
+                v.rotate_left(value as usize % v.len());
+            } else {
+                v.rotate_right((-value) as usize % v.len());
+            }
+            v.push_front((index, value));
+            // println!("                   {v:?}");
+        }
+
+        while v.front().unwrap().1 != 0 {
+            v.rotate_left(1);
+        }
+
+        let v1 = v.get(1000 % v.len()).unwrap().1;
+        let v2 = v.get(2000 % v.len()).unwrap().1;
+        let v3 = v.get(3000 % v.len()).unwrap().1;
+
+        (v1 + v2 + v3).to_string()
     }
 }
 
@@ -63,6 +93,17 @@ mod test {
             }
             .part1(),
             "3"
+        );
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(
+            Day20 {
+                input: INPUT.to_string()
+            }
+            .part2(),
+            "1623178306"
         );
     }
 
